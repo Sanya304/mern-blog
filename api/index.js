e4cd1b15f0118e61ab1965e441a84351c173cabe
Sanya304 <sanya30404@gmail.com>
2024-02-13 15:14:33 +0530

    connecct to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("mongodb is cnnected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 app.listen(3000, () => {
   console.log("server is ruuning at port 3000!");
