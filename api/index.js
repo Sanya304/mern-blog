708b245e2165a27a8dfd253b866537273517baf9
Sanya304 <sanya30404@gmail.com>
2024-02-13 19:28:14 +0530

    create api route

--- api/index.js
+++ api/index.js
@@ -1,16 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+
 dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("mongodb is cnnected");
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`Server is running at port ${PORT}`);
+    });
   })
   .catch((err) => {
-    console.log(err);
+    console.error("MongoDB connection failed:", err.message);
   });
-const app = express();
-app.listen(3000, () => {
-  console.log("server is ruuning at port 3000!");
-});
+
+// Mount the userRoutes onto /api/user
+app.use("/api/user", userRoutes);
